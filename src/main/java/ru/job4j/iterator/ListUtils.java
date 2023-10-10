6272db881853c6b6d3d0fe4a0d137e6eebfabbc2
Kazakov Alexander <doublealex92@yandex.ru>
2023-10-10 19:01:52 +0500

    7. ListIterator [#350217 #451406]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -7,49 +7,39 @@ public class ListUtils {
 
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (iterator.nextIndex() == index) {
-                iterator.add(value);
-                break;
-            }
-            iterator.next();
-        }
+        ListIterator<T> iterator = list.listIterator(index);
+        iterator.add(value);
+        iterator.next();
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (iterator.nextIndex() == index + 1) {
-                iterator.add(value);
-                break;
-            }
-            iterator.next();
-        }
+        ListIterator<T> iterator = list.listIterator(index + 1);
+        iterator.add(value);
+        iterator.next();
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
         ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (filter.test(iterator.next())) {
-                iterator.remove();
-            }
+        if (filter.test(iterator.next())) {
+            iterator.remove();
         }
     }
 
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
         ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (filter.test(iterator.next())) {
-                iterator.set(value);
-            }
+        if (filter.test(iterator.next())) {
+            iterator.set(value);
         }
     }
 
+    /**
+     * Метод удаляет из списка list те элементы, которые содержатся в списке elements
+     * Вызываем у list метод removeIf с указанием в параметрах ссылки на метод: "удалить, если содержится в elements"
+     * @param list - редактируемый список
+     * @param elements - список элементов, которые нужно удалить из списка list
+     */
     public static <T> void removeAll(List<T> list, List<T> elements) {
-            list.removeIf(x -> list.contains(elements));
-        }
-
+            list.removeIf(elements::contains);
     }
-
+}
