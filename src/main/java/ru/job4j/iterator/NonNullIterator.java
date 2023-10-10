7a67fb898cdfceb813b109e6d6efa60d9dbb025e
Kazakov Alexander <doublealex92@yandex.ru>
2023-10-10 22:46:17 +0500

    5. Итератор не-null значений [#505081]

---
+++ src/main/java/ru/job4j/iterator/NonNullIterator.java
@@ -0,0 +1,36 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class NonNullIterator implements Iterator<Integer> {
+
+    private Integer[] data;
+    private int index;
+
+    public NonNullIterator(Integer[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean rsl = false;
+        for (int i = index; i < data.length; i++) {
+            if (data[i] != null) {
+                rsl = true;
+                break;
+            }
+            index++;
+        }
+        return rsl;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[index++];
+    }
+
+}
