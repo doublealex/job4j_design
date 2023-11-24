5a59ee9adca3717eda22865642e0270e863863a7
Kazakov Alexander <doublealex92@yandex.ru>
2023-11-24 19:11:17 +0500

    7. Анализ доступности сервера [#859 #462147]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -8,12 +8,14 @@ public class Analysis {
         try (BufferedReader read = new BufferedReader(new FileReader(source));
         PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (String line = read.readLine(); line != null; line = read.readLine()) {
+                String[] elements = line.split(" ");
                 if (!server && (line.contains("400") || line.contains("500"))) {
                     server = true;
-                    out.print(line.split(" ")[1] + "; ");
+                    out.write(String.format("%s; ", elements[1]));
                 } else if (server && (line.contains("200") || line.contains("300"))) {
                     server = false;
-                    out.println(line.split(" ")[1] + ";");
+                    out.write(String.format("%s;", elements[1]));
+                    out.println();
                 }
             }
         } catch (IOException e) {
