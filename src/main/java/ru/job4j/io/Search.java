274080af4db7e7b71ad0fd6b1ca07724348b5bf5
Kazakov Alexander <doublealex92@yandex.ru>
2023-11-28 18:39:28 +0500

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,27 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Must be 2 arguments.");
+        }
+        Path start = Paths.get(args[0]);
+        if (!Files.isDirectory(start)) {
+            throw new IllegalArgumentException("First argument must be a folder.Root folder does not exist");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Second argument must be file extension and starts with dot.");
+        }
     }
 }
