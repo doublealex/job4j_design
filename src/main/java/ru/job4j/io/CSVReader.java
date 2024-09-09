8810b6ff5d80e92a9957f6adbde7c463fdb7156b
Kazakov Alexander <doublealex92@yandex.ru>
2024-09-09 19:28:04 +0500

    17. Scanner [#504791 #512046]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -5,7 +5,13 @@ import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.util.*;
 
-/* -path=file.csv -delimiter=; -out=stdout -filter=name,age */
+/**
+ *  Для корректной работы обязательно ввести следующие параметры запуска:
+ *  путь к CSV файлу; разделитель; вывод(консоль, либо запись в другой файл); фильтр(заголовки столбцов).
+ *  Пример:
+ *  -path=file.csv -delimiter=; -out=stdout -filter=name,age
+ *  Если фильтрация не нужна, необходимо ввести заголовки всех столбцов.
+ */
 
 public class CSVReader {
     private static final String PATH = "path";
@@ -13,6 +19,7 @@ public class CSVReader {
     private static final String OUT = "out";
     private static final String FILTER = "filter";
 
+    /** Метод получает список из метода filterCSV и по строкам выводит его в консоль, либо записывает в файл*/
     public static void handle(ArgsName argsName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(argsName.get("out")))) {
             List<String> filteredData = filterCSV(argsName);
@@ -22,63 +29,61 @@ public class CSVReader {
                     System.out.println(line);
                 }
             } else {
-                writer.write((filterCSV(argsName) + System.lineSeparator()));
+                for (String line : filteredData) {
+                    writer.write(line + System.lineSeparator());
+                }
             }
         } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
+    /** Метод фильтрует данные из CSV файла в соответствии с параметром filter и возвращает отфильтрованный список */
     public static List<String> filterCSV(ArgsName argsName) {
-        List<String> filteredValues = new ArrayList<>();
+        List<String> filtered = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(argsName.get("path")))) {
-            String[] filters = argsName.get("filter").split(",");
+            /* Читаем первую строку из CSV файла и создаем Map с названием всех заголовков */
             String header  = reader.readLine();
             Scanner headerScanner = new Scanner((new ByteArrayInputStream(header.getBytes())))
                     .useDelimiter(argsName.get("delimiter"));
-
             Map<String, Integer> headerIndexMap = new HashMap<>();
             int index = 0;
-            /* Считываем заголовки */
             while (headerScanner.hasNext()) {
                 String head = headerScanner.next();
                 headerIndexMap.put(head, index++);
             }
-
-            for (String filter : filters) {
-                if (headerIndexMap.containsKey(filter)) {
-                    filteredValues.add(filter);
-                }
-            }
-
-            /* Чтение и вывод данных, соответствующих заголовкам из filter */
+            /* Добавляем заголовки переданные в filter в список filtered в формате, как они представлены в CSV файле */
+            filtered.add(argsName.get("filter").replace(",", argsName.get("delimiter")));
+            /* Читаем строки CSV файла, записываем в массив values значения из строк CSV файла */
             String line;
             while ((line = reader.readLine()) != null) {
                 Scanner lineScanner = new Scanner((new ByteArrayInputStream(line.getBytes())))
                         .useDelimiter(argsName.get("delimiter"));
-                List<String> selected = new ArrayList<>();
                 String[] values = new String[headerIndexMap.size()];
                 int colIndex = 0;
                 while (lineScanner.hasNext()) {
                     values[colIndex++] = lineScanner.next();
                 }
-
-                /* Собираем те значения, которые соответствуют фильтрам */
+                /* Добавляем из массива values в промежуточный список selected данные, соответствующие параметру filter,
+                 затем добавляем selected в список filtered в нужным разделителем */
+                String[] filters = argsName.get("filter").split(",");
+                List<String> selected = new ArrayList<>();
                 for (String filter : filters) {
                     Integer columnIndex = headerIndexMap.get(filter);
                     if (columnIndex != null && columnIndex < values.length) {
                         selected.add(values[columnIndex]);
                     }
                 }
-                filteredValues.add(String.join(",", selected));
+                filtered.add(String.join(argsName.get("delimiter"), selected));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return filteredValues;
+        return filtered;
     }
 
+    /** Метод валидации аргументов(параметров), заданных при запуске класса CSVReader */
     private static void validateArgsForReader(ArgsName argsName) {
         String path = argsName.get(PATH);
         if (path == null) {
@@ -108,10 +113,7 @@ public class CSVReader {
         }
     }
 
-
-
     public static void main(String[] args) {
-        /* здесь добавьте валидацию принятых параметров*/
         ArgsName argsName = ArgsName.of(args);
         validateArgsForReader(argsName);
         handle(argsName);
